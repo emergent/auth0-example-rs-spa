bb623d30cd5aeefbeb9be53f071963377cacec1d
emergent <yosikawa@gmail.com>
2023-01-08 03:54:18 +0900

    replace hero from React to Yew

--- src/component/hero.rs
+++ src/component/hero.rs
@@ -10,12 +10,12 @@ pub fn hero(props: &HeroProps) -> Html {
 
     html! {
         <div class={classes!("text-center", "hero", "my-5")}>
-        <img class={classes!("mb-3", "app-logo")} src={logo} alt={"React logo"} width={120} />
-        <h1 class={classes!("mb-4")}>{ "React.js Sample Project" }</h1>
+        <img class={classes!("mb-3", "app-logo")} src={logo} alt={"Yew logo"} width={120} />
+        <h1 class={classes!("mb-4")}>{ "Yew Sample Project" }</h1>
 
         <p class={classes!("lead")}>
           { "This is a sample application that demonstrates an authentication flow for
-          an SPA, using "}<a href={"https://reactjs.org"}>{"React.js"}</a>
+          an SPA, using "}<a href={"https://yew.rs/"}>{"Yew"}</a>
         </p>
       </div>
     }
